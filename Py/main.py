21569b162b8a4ca2dd1ae28ad23090e6f643ba84
Sachin Raj <sachinr@maqsoftware.com>
2024-01-17 14:08:12 +0530

    Added tabulator and changes api output to share output data.

--- Py/main.py
+++ Py/main.py
@@ -2,10 +2,24 @@ from typing import Union
 from utils import sample
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS configuration
+origins = [
+    "http://localhost",  # Add the URLs of your frontend applications
+    "http://localhost:3000",  # If your frontend is running on a different port
+    "https://yourfrontenddomain.com",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify specific HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # You can specify specific headers if needed
+)
 @app.get("/")
 def read_root():
     return sample.outputFun()
